e94b85f0b02880f0b41c2c13e7e2edc90b5e127d
Harsh Parekh <harsh_parekh@outlook.com>
2021-01-02 22:26:10 -0500

    Remove battery_perc status.

--- config.def.h
+++ config.def.h
@@ -65,6 +65,5 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function 	format          argument */
-	{ battery_perc, " 🔋 %s\% |",   "BAT0" },
 	{ datetime, 	" %s",          "%a %d %b %I:%M %p" },
 };
